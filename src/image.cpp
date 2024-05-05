ea41a105149845cec64105d74af996f6ed72d817
Philipp Weber <philipp.ph.weber@fau.de>
2024-05-05 16:59:27 +0200

    Add debayering

--- src/image.cpp
+++ src/image.cpp
@@ -1,45 +1,22 @@
 #include "image.h"
+#include <memory>
 
 namespace ELB {
 
-std::string Image::fitsError(int status) {
-	char buff[256];
-	if ( status == 0 ) {
-		throw std::runtime_error("0 is not a fits error code");
+int FFPtr::bayerNameToValue(const std::string &name) {
+	static std::map<std::string, int> map = {
+		{ "RGGB", cv::COLOR_BayerRG2BGR },
+		{ "GRBG", cv::COLOR_BayerGR2BGR },
+		{ "BGGR", cv::COLOR_BayerBG2BGR },
+		{ "GBRG", cv::COLOR_BayerGB2BGR },
+	};
+	if ( ! map.count(name) ) {
+		throw std::runtime_error(std::string("Unsupported Bayer pattern: ") + name);
 	}
-	fits_get_errstatus(status, buff);
-	return std::string(buff);
+	return map[name];
 }
 
-Image::Image(const std::string &fileName) : m_fileName(fileName) {
-	long fpix[2] = {1,1};
-	long lpix[2];
-	long inc[2] = {1,1};
-	int bitpix = 0;
-	pixfloat nullval = 0;
-	int anynull = 0;
-
-	FFPtr ffptr(fileName);
-	ffptr.read_key("NAXIS1", TLONG, &m_dimX, NULL);
-	ffptr.read_key("NAXIS2", TLONG, &m_dimY, NULL);
-	lpix[0] = m_dimX;
-	lpix[1] = m_dimY;
-	m_nPix = m_dimX * m_dimY;
-	ffptr.read_key("BITPIX", TINT, &bitpix, NULL);
-	m_data = std::make_unique<pixfloat[]>(m_nPix);
-	ffptr.read_subset(fitsfloat, fpix, lpix, inc, &nullval, m_data.get(), &anynull);
-
-    pixfloat * __restrict__ data = m_data.get();
-    // bitpix larger 0 means integer, so rescale values
-    if ( bitpix > 0 ) {
-        m_scale = 1. / (1<<bitpix);
-        for (long ii=0; ii<m_nPix; ii++) {
-            data[ii] = m_scale * m_data[ii];
-        }
-    }
-}
-
-std::string Image::FFPtr::fitsError() {
+std::string FFPtr::fitsError() {
     if ( m_status == 0 ) {
         throw std::runtime_error("0 is not a fits error code");
     }
@@ -48,91 +25,151 @@ std::string Image::FFPtr::fitsError() {
     return std::string(text);
 }
 
-void Image::FFPtr::check() {
+void FFPtr::check() {
     check("Error: ");
 }
 
-void Image::FFPtr::check(const std::string &message) {
+void FFPtr::check(const std::string &message) {
     if ( m_status == 0 ) {
         return;
     }
-    throw std::runtime_error(message + ": " + fitsError() + " (" + m_fname + ")");
+    throw FitsError(message + ": " + fitsError() + " (" + m_fname + ")", m_status);
 }
 
-Image::FFPtr::FFPtr(const std::string &fname) {
+FFPtr::FFPtr(const std::string &fname) {
     m_fname = fname;
     fits_open_file(&m_ffptr, fname.c_str(), READONLY, &m_status);
     check("Error opening file");
+
+	long fpix[2] = {1,1};
+	long lpix[2];
+	long inc[2] = {1,1};
+	int bitpix = 0;
+	pixfloat nullval = 0;
+	int anynull = 0;
+
+	// X and Y is swapped for FITS
+	read_key("NAXIS1", TLONG, &m_dimY, NULL);
+	read_key("NAXIS2", TLONG, &m_dimX, NULL);
+	lpix[0] = m_dimY;
+	lpix[1] = m_dimX;
+	m_nPix = m_dimX * m_dimY;
+	read_key("BITPIX", TINT, &bitpix, NULL);
+	auto rawData = std::make_unique<pixfloat[]>(m_nPix);
+	read_subset(fitsfloat, fpix, lpix, inc, &nullval, rawData.get(), &anynull);
+
+	auto intData = std::make_unique<uint16_t[]>(m_nPix);
+    m_scale = 1. / (1<<bitpix) * (1<<16);
+	auto * __restrict__ __rawData = rawData.get();
+	auto * __restrict__ __intData = intData.get();
+	for ( long ii=0; ii<m_nPix; ii++ ) {
+		__intData[ii] = __rawData[ii] * m_scale;
+	}
+	rawData.reset();
+	m_data = std::make_unique<cv::Mat>(m_dimX, m_dimY, cvfloat, intData.get());
+	intData.reset();
+	debayerIfNecessary();
 }
 
-Image::FFPtr::FFPtr(fitsfile *ffptr, int status, const std::string &fname) {
-    m_fname = fname;
-    m_ffptr = ffptr;
-    m_status = status;
-    check("Error creating file");
+void FFPtr::debayerIfNecessary() {
+	try {
+		char buff[32];
+		read_key("BAYERPAT", TSTRING, &buff, NULL);
+		m_bayerPat = buff;
+	} catch ( const FFPtr::FitsError &e ) {
+		resetStatus();
+		return;
+	}
+	int pattern = bayerNameToValue(m_bayerPat);
+	auto debayered = std::make_unique<cv::Mat>();
+	cv::cvtColor(*m_data.get(), *debayered, pattern);
+	m_data = std::move(debayered);
 }
 
-Image::FFPtr::~FFPtr() {
+FFPtr::~FFPtr() {
     fits_close_file(m_ffptr, &m_status);
     check("Error closing file");
 }
 
-void Image::FFPtr::read_key(const std::string &key, int datatype, void *value,
+void FFPtr::read_key(const std::string &key, int datatype, void *value,
         char *comment) {
     fits_read_key(m_ffptr, datatype, key.c_str(), value, comment, &m_status);
     check("Error reading key " + key);
 }
 
-void Image::FFPtr::write_key(int datatype, const std::string &key, void *value,
+void FFPtr::write_key(int datatype, const std::string &key, void *value,
         char *comment) {
     fits_write_key(m_ffptr, datatype, key.c_str(), value, comment, &m_status);
     check("Error writing key " + key);
 }
 
-void Image::FFPtr::read_subset(int datatype, long *fpix, long *lpix, long *inc,
+void FFPtr::read_subset(int datatype, long *fpix, long *lpix, long *inc,
         void *nullval, void *data, int *anynull) {
     fits_read_subset(m_ffptr, datatype, fpix, lpix, inc, nullval, data,
             anynull, &m_status);
     check("Error reading pixel data");
 }
 
-void Image::FFPtr::get_hdrspace(int *numKeys, int *moreKeys) {
+void FFPtr::get_hdrspace(int *numKeys, int *moreKeys) {
     fits_get_hdrspace(m_ffptr, numKeys, moreKeys, &m_status);
     check("Error getting header size");
 }
 
-void Image::FFPtr::read_record(int ii, char *buffer) {
+void FFPtr::read_record(int ii, char *buffer) {
     fits_read_record(m_ffptr, ii, buffer, &m_status);
     check("Error reading header record");
 }
 
-void Image::FFPtr::read_keyn(int ii, char *name, char *value, char *comment) {
+void FFPtr::read_keyn(int ii, char *name, char *value, char *comment) {
     fits_read_keyn(m_ffptr, ii, name, value, comment, &m_status);
     check("Error reading key number " + std::to_string(ii));
 }
 
-void Image::FFPtr::create_img(int bitpix, int naxis, long *naxes) {
+void FFPtr::create_img(int bitpix, int naxis, long *naxes) {
     fits_create_img(m_ffptr, bitpix, naxis, naxes, &m_status);
     check("Error creating image HDU");
 }
 
-void Image::FFPtr::write_record(const char *card) {
+void FFPtr::write_record(const char *card) {
     fits_write_record(m_ffptr, card, &m_status);
     check("Error writing card");
 }
 
-void Image::FFPtr::write_img(int datatype, LONGLONG firstelement,
+void FFPtr::write_img(int datatype, LONGLONG firstelement,
         LONGLONG nelements, void *data) {
     fits_write_img(m_ffptr, datatype, firstelement, nelements, data, &m_status);
     check();
 }
 
-int Image::FFPtr::status() {
+int FFPtr::status() {
     return m_status;
 }
 
-void Image::FFPtr::resetStatus() {
+void FFPtr::resetStatus() {
     m_status = 0;
+}
+
+pixfloat FFPtr::mean() {
+	pixfloat mean = 0.0;
+	for ( long ii=0; ii<m_dimX; ii++ ) {
+		for ( long jj=0; jj<m_dimY; jj++ ) {
+			mean += m_data->at<pixfloat>(ii,jj);
+		}
+	}
+	return mean / m_nPix;
+}
+
+FFPtr::FitsError::FitsError(const std::string &message, int status) : std::runtime_error(message) {
+	m_status = status;
+	m_message = message;
+}
+
+const char *FFPtr::FitsError::what() const noexcept {
+	return m_message.c_str();
+}
+
+int FFPtr::FitsError::status() const noexcept {
+	return m_status;
 }
 
 }
--- src/image.h
+++ src/image.h
@@ -3,30 +3,36 @@
 #include <iostream>
 #include <stdexcept>
 #include <memory>
+#include <map>
+
 #include <fitsio.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 #define STRBUFF (256)
 
 #define pixfloat float
 #define fitsfloat TFLOAT
+#define cvfloat CV_16U
 
 namespace ELB {
 
-class Image {
-	public:
-		Image(const std::string &fileName);
-	private:
-		std::string fitsError(int status);
-
-		std::string m_fileName;
-		long m_dimX, m_dimY, m_nPix;
-		std::unique_ptr<pixfloat[]> m_data;
-		pixfloat m_scale = 0;
-
     class FFPtr {
         public:
+			class FitsError : public std::runtime_error {
+				public:
+					FitsError(const std::string &message, int status);
+					virtual const char* what() const noexcept override;
+					int status() const noexcept;
+				private:
+					int m_status = 0;
+					std::string m_message;
+			};
+			static int bayerNameToValue(const std::string &name);
+
             FFPtr(const std::string &fname);
-            FFPtr(fitsfile *ffptr, int status, const std::string &fname);
             ~FFPtr();
             void read_key(const std::string &key, int datatype, void *value,
                     char *comment);
@@ -45,13 +51,17 @@ class Image {
             void write_record(const char *card);
             void write_img(int datatype, LONGLONG firstelement,
                     LONGLONG nelements, void *data);
+			pixfloat mean();
         private:
+			void debayerIfNecessary();
+			void stretch();
             std::string fitsError();
             std::string m_fname;
             fitsfile *m_ffptr = NULL;
             int m_status = 0;
+			long m_dimX, m_dimY, m_nPix;
+			double m_scale;
+			std::unique_ptr<cv::Mat> m_data;
+			std::string m_bayerPat = "";
     };
-};
-
-
 }
